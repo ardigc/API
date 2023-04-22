95a10c21c1e6a754a7366da95084b5e547c65bcd
Juan Martin Olivas <juanmarolivas@gmail.com>
2023-04-22 17:59:19 +0200

    Serve files and connect be and fe

--- api.js
+++ api.js
@@ -1,1 +1,52 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs/promises');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', async (req, res) => {
+    const pathName = path.join(__dirname, 'index.html');
+    const file = await fs.readFile(pathName);
+    res.statusCode = 200;
+    res.setHeader('content-type', 'text/html');
+    res.send(file);
+});
+
+app.get('/script.js', async (req, res) => {
+    const pathName = path.join(__dirname, 'script.js');
+    const file = await fs.readFile(pathName);
+    res.statusCode = 200;
+    res.setHeader('content-type', 'application/javascript');
+    res.send(file);
+});
+
+app.get('/api/message', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization'
+    );
+    res.send('Mandado desde la api');
+});
+
+app.post('/api/save', (req, res) => {
+    console.log(req.body);
+    res.statusCode = 200;
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, OPTIONS'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization'
+    );
+    res.send('Mandado desde la api');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const button = document.querySelector('.btn');
+const saveBtn = document.querySelector('.save');
 
-button.addEventListener('click', () => {
-    alert('hey');
+button.addEventListener('click', async () => {
+    const request = await fetch('/api/message');
+    const message = await request.text();
+    alert(message);
+});
+
+saveBtn.addEventListener('click', async () => {
+    const request = await fetch('/api/save', {
+        method: 'POST',
+        body: JSON.stringify({ message: 'Mensaje enviado desde el front' }),
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        },
+    });
+    const message = await request.text();
+    console.log(message);
 });
